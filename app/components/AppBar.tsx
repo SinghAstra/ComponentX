5409b697ef0ba0f9312fe484ae5686c4b49fbf7a
Astra <abhaypratapsinghwd@gmail.com>
2024-09-19 10:51:27 +0530

    Schema design

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 const AppBar = () => {
@@ -11,9 +11,17 @@ const AppBar = () => {
   return (
     <div className="flex justify-between">
       <h2>Live Music</h2>
-      <button className="bg-blue-400 p-2 m-2" onClick={() => signIn()}>
-        Sign In
-      </button>
+      {session.status === "loading" && <p>Loading....</p>}
+      {session.status === "authenticated" && (
+        <button className="bg-blue-400 p-2 m-2" onClick={() => signOut()}>
+          Sign Out
+        </button>
+      )}
+      {session.status === "unauthenticated" && (
+        <button className="bg-blue-400 p-2 m-2" onClick={() => signIn()}>
+          Sign In
+        </button>
+      )}
     </div>
   );
 };
