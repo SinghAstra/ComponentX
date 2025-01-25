e289f3870ea767fc489030971e6062811714cd01
Astra <abhaypratapsinghwd@gmail.com>
2025-01-25 16:16:07 +0530

    css in mdx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { allDocs } from "@/.contentlayer/generated";
+import { Mdx } from "@/components/mdx-components";
+import { badgeVariants } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { ChevronRight, ExternalLink } from "lucide-react";
 import { useMDXComponent } from "next-contentlayer2/hooks";
+import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
 const DocsPage = () => {
   console.log("allPosts is ", allDocs);
@@ -11,10 +17,57 @@ const DocsPage = () => {
   console.log("MDXContent is ", MDXContent);
 
   return (
-    <main className="flex-1 md:ml-64 p-4">
-      <article className="prose lg:prose-xl">
-        <MDXContent />
-      </article>
+    <main className="relative py-6 flex-1 md:ml-64 ">
+      <div className="mx-auto w-full max-w-2xl">
+        <div className="mb-4 flex items-center space-x-1 text-sm leading-none text-muted-foreground">
+          <div className="truncate">Docs</div>
+          <ChevronRight className="h-3.5 w-3.5" />
+          <div className="text-foreground">{doc.title}</div>
+        </div>
+        <div className="space-y-2">
+          <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
+            {doc.title}
+          </h1>
+          {doc.description && (
+            <p className="text-base text-muted-foreground">
+              <Balancer>{doc.description}</Balancer>
+            </p>
+          )}
+        </div>
+        {doc.links ? (
+          <div className="flex items-center space-x-2 pt-4">
+            {doc.links?.doc && (
+              <Link
+                href={doc.links.doc}
+                target="_blank"
+                rel="noreferrer"
+                className={cn(badgeVariants({ variant: "secondary" }), "gap-1")}
+              >
+                Docs
+                <ExternalLink className="h-3 w-3" />
+              </Link>
+            )}
+            {doc.links?.api && (
+              <Link
+                href={doc.links.api}
+                target="_blank"
+                rel="noreferrer"
+                className={cn(badgeVariants({ variant: "secondary" }), "gap-1")}
+              >
+                API Reference
+                <ExternalLink className="h-3 w-3" />
+              </Link>
+            )}
+          </div>
+        ) : null}
+        <div className="pb-12 pt-8">
+          <Mdx code={doc.body.code} />
+        </div>
+        {/* <DocsPager doc={doc} /> */}
+      </div>
+      <div className="hidden text-sm md:block w-20">
+        {/* {doc.toc && <DashboardTableOfContents toc={toc} />} */}
+      </div>
     </main>
   );
 };
