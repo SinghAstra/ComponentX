8b80d0d3d16caccf961f605e0ce2a5d1573fff57
Astra <abhaypratapsinghwd@gmail.com>
2025-01-26 16:01:31 +0530

    Theming

--- components/callout.tsx
+++ components/callout.tsx
@@ -9,7 +9,7 @@ export function Callout({
   ...props
 }: React.ComponentProps<typeof Alert> & { icon?: string }) {
   return (
-    <Alert className={cn("bg-muted/50 flex ", className)} {...props}>
+    <Alert className={cn("bg-muted/50 flex my-2", className)} {...props}>
       {icon && <span className="mr-2 text-2xl">{icon}</span>}
       {title && <AlertTitle>{title}</AlertTitle>}
       <AlertDescription>{children}</AlertDescription>
