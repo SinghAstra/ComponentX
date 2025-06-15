84527ac638798768a72799cf2cc2a7f5005c853d
Astra <abhaypratapsinghwd@gmail.com>
2025-06-15 11:40:09 +0530

    Background

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { GradientOrbsShowcase } from "./gradient";
+import GradientBackground from "./gradient";
 import { HeroContent } from "./hero";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col  gap-2">
       <HeroContent />
-      <GradientOrbsShowcase />
+      <GradientBackground />
     </div>
   );
 };
