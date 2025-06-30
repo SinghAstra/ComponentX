2146dd599ac1f4219c8a69b59e8b8a7ff0834567
Astra <abhaypratapsinghwd@gmail.com>
2025-06-30 23:38:03 +0530

    Masked Grid

--- app/ui/grid-background.tsx
+++ app/ui/grid-background.tsx
@@ -8,6 +8,8 @@ const GridBackground = () => {
         backgroundImage:
           "linear-gradient(90deg,#161616 1px,transparent 1px),linear-gradient(180deg,#161616 1px,transparent 1px)",
         backgroundSize: "48px 48px",
+        maskImage:
+          "radial-gradient(circle at 50% 50%, rgba(255, 255, 255, 0), rgb(255, 255, 255))",
       }}
     />
   );
