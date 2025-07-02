945aa7e297e9cc7d8eaf09251afc131307091fe8
Astra <abhaypratapsinghwd@gmail.com>
2025-07-02 12:42:32 +0530

    Edge Triad Banner

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import GradientInsetBackgroundShowCase from "./showcase/gradient-inset-backgroun
 import MovingBackgroundShowCase from "./showcase/moving-background-showcase";
 import SearchDialogShowCase from "./showcase/search-dialog-showcase";
 import ConicGradientBackground from "./ui/conic-gradient-background";
+import EdgeTriadBanner from "./ui/edge-triad-banner";
 import GridBackground from "./ui/grid-background";
 import LightBulbGradient from "./ui/light-bulb-gradient";
 import MaskedGridBackground from "./ui/masked-grid-background";
 import NotificationInput from "./ui/notification-input";
 import RadialFadeBackground from "./ui/radial-fade-background";
 import RadialFadePulsatingBackground from "./ui/radial-fade-pulsating-background";
+import ReverseEdgeTriadBanner from "./ui/reverse-edge-triad-banner";
 import ReverseTriadBanner from "./ui/reverse-triad-banner";
 import TriadBanner from "./ui/triad-banner";
 
@@ -113,6 +115,25 @@ const HomePage = () => {
             Masked Grid
           </motion.h1>
           <MaskedGridBackground />
+        </div>
+
+        <div className="col-span-1 sm:col-span-2 row-span-2 min-h-[250px]  border border-neutral-800 rounded relative flex items-center justify-center">
+          <EdgeTriadBanner />
+          <motion.h1
+            className="text-2xl sm:text-4xl font-normal text-center"
+            variants={textVariant}
+          >
+            Edge Triad Banner
+          </motion.h1>
+        </div>
+        <div className="col-span-1 sm:col-span-2 row-span-2 min-h-[250px]  border border-neutral-800 rounded relative flex items-center justify-center">
+          <ReverseEdgeTriadBanner />
+          <motion.h1
+            className="text-2xl sm:text-4xl font-normal text-center"
+            variants={textVariant}
+          >
+            Reverse Edge Triad Banner
+          </motion.h1>
         </div>
 
         <div className="col-span-1  row-span-3 min-h-[250px] relative flex items-center justify-center border border-neutral-800 rounded">
---
+++ app/ui/edge-triad-banner.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const EdgeTriadBanner = () => {
+  return (
+    <div className="absolute inset-0 ">
+      <div className="relative w-full h-full grid grid-cols-12">
+        <div
+          className="col-span-3 "
+          style={{
+            maskImage:
+              "radial-gradient(circle at 100% 50%, rgb(255, 255, 255,0.6), rgba(255, 255, 255,0))",
+            clipPath: "polygon(0 0, 100% 0%, 70% 100%, 0% 100%)",
+            opacity: "0.5",
+            background: `linear-gradient(100deg, hsl(var(--primary)/0.3) , hsl(var(--primary)) )`,
+          }}
+        />
+        <div className="col-span-6"></div>
+        <div
+          className="col-span-3 "
+          style={{
+            maskImage:
+              "radial-gradient(circle at 0% 50%, rgb(255, 255, 255,0.6), rgba(255, 255, 255,0))",
+            clipPath: "polygon(30% 0, 100% 0%, 100% 100%, 0% 100%)",
+            opacity: "0.5",
+            background: `linear-gradient(100deg, hsl(var(--primary)), hsl(var(--primary)/0.3) )`,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EdgeTriadBanner;
