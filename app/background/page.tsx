757d9efbd2af90062aac02a68e625042949f013e
Astra <abhaypratapsinghwd@gmail.com>
2025-07-12 10:23:05 +0530

    Background Individual Element

--- app/background/page.tsx
+++ app/background/page.tsx
@@ -2,24 +2,31 @@
 
 import { blurInVariant, containerVariant } from "@/lib/variants";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
 import ConicGradientBackground from "../ui/conic-gradient-background";
 
 const BackgroundPage = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 auto-rows-fr p-4 sm:p-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 auto-rows-fr p-4 sm:p-8">
       <motion.div
         variants={containerVariant}
         initial="hidden"
         whileInView="visible"
-        className="col-span-1 sm:col-span-2 row-span-2 min-h-[250px] flex items-center justify-center border border-neutral-800 rounded relative"
+        className="col-span-1 sm:col-span-2 row-span-2 min-h-[250px] flex flex-col gap-4 items-center justify-center border border-neutral-800 rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
       >
         <motion.h1
-          className="text-2xl sm:text-4xl font-normal text-center"
+          className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
           variants={blurInVariant}
         >
           Conic Gradient Background
         </motion.h1>
+        <Link
+          href="/background/conic-gradient-background"
+          className="border px-3 py-2 rounded bg-muted/20 hover:bg-muted/40 backdrop-blur-md translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-200 "
+        >
+          View in Action
+        </Link>
         <ConicGradientBackground />
       </motion.div>
     </div>
