a09df42d08e3c5ed3eba1203fd349590d2d5f200
Astra <abhaypratapsinghwd@gmail.com>
2025-07-19 18:14:24 +0530

    Home Page Responsive

--- app/home.tsx
+++ app/home.tsx
@@ -40,7 +40,7 @@ const HomePage = () => {
           variants={containerVariant}
           initial="hidden"
           whileInView="visible"
-          className="bg-background col-span-1 row-span-1 min-h-[250px] flex items-center justify-center border rounded"
+          className="bg-background col-span-1 sm:col-span-2 lg:col-span-1 row-span-1 min-h-[250px] flex items-center justify-center border rounded"
         >
           <FloatingNavbar showNavbar={showFloatingNavbar} />
           <motion.div variants={scaleInVariant}>
@@ -67,7 +67,7 @@ const HomePage = () => {
           variants={containerVariant}
           initial="hidden"
           whileInView="visible"
-          className="col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+          className="col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
         >
           <motion.h1
             className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
@@ -88,7 +88,7 @@ const HomePage = () => {
           variants={containerVariant}
           initial="hidden"
           whileInView="visible"
-          className="col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+          className="col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
         >
           <motion.h1
             className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
@@ -138,7 +138,7 @@ const HomePage = () => {
           variants={containerVariant}
           initial="hidden"
           whileInView="visible"
-          className="col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+          className="col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
         >
           <motion.h1
             className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
@@ -159,7 +159,7 @@ const HomePage = () => {
           variants={containerVariant}
           initial="hidden"
           whileInView="visible"
-          className="col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+          className="col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
         >
           <motion.h1
             className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
@@ -189,41 +189,56 @@ const HomePage = () => {
             </div>
           </div>
         </div>
+        <motion.div
+          variants={containerVariant}
+          initial="hidden"
+          whileInView="visible"
+          className=" col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+        >
+          <motion.h1
+            className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
+            variants={blurInVariant}
+          >
+            Grid Background
+          </motion.h1>
+          <Link
+            href="/background/grid-background"
+            className="border px-3 py-1 rounded bg-muted/20 hover:bg-muted/40 backdrop-blur-md translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-200 flex gap-1 items-center"
+          >
+            View in Action
+            <ArrowRightIcon className="size-4 transform-all duration-300 group-hover:translate-x-1" />
+          </Link>
+          <GridBackground />
+        </motion.div>
 
-        <div className="col-span-1 row-span-1 min-h-[250px] flex items-center justify-center border border-neutral-800 rounded relative bg-background">
+        <motion.div
+          variants={containerVariant}
+          initial="hidden"
+          whileInView="visible"
+          className=" col-span-1 sm:col-span-2 row-span-1 flex flex-col gap-4 items-center justify-center border rounded relative group transition-all duration-200 hover:shadow-[inset_0_-5px_30px_0px_hsl(var(--muted)),inset_0px_5px_30px_0px_hsl(var(--muted))] hover:bg-background/60"
+        >
+          <motion.h1
+            className="text-2xl sm:text-4xl font-normal text-center translate-y-5 group-hover:translate-y-0 transition-all duration-200 "
+            variants={blurInVariant}
+          >
+            Masked Grid Background
+          </motion.h1>
+          <Link
+            href="/background/masked-grid-background"
+            className="border px-3 py-1 rounded bg-muted/20 hover:bg-muted/40 backdrop-blur-md translate-y-10 group-hover:translate-y-0 opacity-0 group-hover:opacity-100 transition-all duration-200 flex gap-1 items-center"
+          >
+            View in Action
+            <ArrowRightIcon className="size-4 transform-all duration-300 group-hover:translate-x-1" />
+          </Link>
+          <MaskedGridBackground />
+        </motion.div>
+
+        <div className="col-span-1  row-span-1 min-h-[250px] flex items-center justify-center border border-neutral-800 rounded relative bg-background">
           <AuthModalShowCase />
         </div>
         <div className="col-span-1 row-span-1 min-h-[250px] flex items-center justify-center border border-neutral-800 rounded bg-background">
           <NotificationInput />
         </div>
-        <motion.div
-          variants={containerVariant}
-          initial="hidden"
-          whileInView="visible"
-          className="col-span-1 row-span-1 relative min-h-[250px] flex items-center justify-center border border-neutral-800 rounded px-3 py-2"
-        >
-          <GridBackground />
-          <motion.h1
-            className="text-2xl sm:text-3xl font-normal text-center "
-            variants={blurInVariant}
-          >
-            Grid Background
-          </motion.h1>
-        </motion.div>
-        <motion.div
-          variants={containerVariant}
-          initial="hidden"
-          whileInView="visible"
-          className="col-span-1 row-span-1 relative min-h-[250px] flex items-center justify-center border border-neutral-800 rounded px-3 py-2"
-        >
-          <MaskedGridBackground />
-          <motion.h1
-            className="text-2xl sm:text-3xl font-normal text-center "
-            variants={blurInVariant}
-          >
-            Masked Grid Background
-          </motion.h1>
-        </motion.div>
       </div>
     </motion.div>
   );
--- app/ui/moving-border.tsx
+++ app/ui/moving-border.tsx
@@ -7,7 +7,7 @@ const MovingBorder = () => {
       className="w-[2000%] h-[2000%] absolute moving-glow z-[-1]"
       style={{
         background:
-          "conic-gradient(from 0deg at 50% 50%,hsl(var(--background)) 0deg,hsl(var(--primary)) 20deg,hsl(var(--background)) 90deg)",
+          "conic-gradient(from 0deg at 50% 50%,transparent 0deg,hsl(var(--primary)) 20deg,transparent 90deg)",
       }}
     />
   );
