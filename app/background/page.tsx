5876db22b68f561f54571412bf02d615bf5f2dd5
Astra <abhaypratapsinghwd@gmail.com>
2025-08-31 23:45:32 +0530

    Fixed deployment Bug

--- app/background/page.tsx
+++ app/background/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
+import ConicBackground from "@/components/component-x/conic-background";
+import EllipseBackground from "@/components/component-x/ellipse-background";
+import GridBackground from "@/components/component-x/grid-background";
+import LampBackground from "@/components/component-x/lamp-background";
+import MaskedGridBackground from "@/components/component-x/masked-grid-background";
+import RadialBackground from "@/components/component-x/radial-background";
 import { blurInVariant, containerVariant } from "@/lib/variants";
 import { motion } from "framer-motion";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import ConicBackground from "../ui/conic-background";
-import EllipseBackground from "../ui/ellipse-background";
-import GridBackground from "../ui/grid-background";
-import LampBackground from "../ui/lamp-background";
-import MaskedGridBackground from "../ui/masked-grid-background";
-import RadialBackground from "../ui/radial-background";
 
 const BackgroundPage = () => {
   return (
