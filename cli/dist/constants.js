afd68b1395eb4957aa9c9b2695c076aaa27b6c8c
Astra <abhaypratapsinghwd@gmail.com>
2025-11-04 18:40:29 +0530

    CLi

---
+++ cli/dist/constants.js
@@ -0,0 +1,2 @@
+export const CONFIG_FILE_NAME = "cx.json";
+export const COMPONENT_SOURCE_BASE = "https://raw.githubusercontent.com/SinghAstra/ComponentX/main/www/components/component-x";
--- cli/types.ts
+++ cli/types.ts
@@ -1,18 +1,3 @@
 export interface Config {
   directory: string;
-  aliases: {
-    [key: string]: string;
-  };
-  configVersion: string;
-}
-
-export interface ComponentSource {
-  name: string;
-  content: string;
-}
-
-export interface ComponentMetadata {
-  name: string;
-  version: string;
-  dependencies: string[];
 }
