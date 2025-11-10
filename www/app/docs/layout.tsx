cf1ab569cddd4b39a35f39a494b0c10d40e7847c
Astra <abhaypratapsinghwd@gmail.com>
2025-11-10 11:18:08 +0530

    Navbar

--- www/app/docs/layout.tsx
+++ www/app/docs/layout.tsx
@@ -12,31 +12,23 @@ import TableOfContents from "../../components/component-x/toc";
 const DocsLayout = ({ children }: { children: React.ReactNode }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex items-center w-full border border-yellow-400">
-        <Button
-          variant="ghost"
-          className="hover:bg-transparent md:hidden"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        >
-          <Menu />
-        </Button>
-        <Link
-          href="/"
-          className="font-medium leading-loose text-2xl text-primary"
-        >
+    <div className="h-screen flex flex-col overflow-hidden">
+      <div className="sticky top-0 inset-x-0 flex items-center justify-between bg-deep-primary p-4">
+        <Link href="/" className="font-medium text-2xl text-primary">
           {siteConfig.name}
         </Link>
-        <a
-          href={siteConfig.links.github}
-          target="_blank"
-          className="inline-flex items-center justify-center rounded transition-all duration-200 hover:bg-muted/80 py-2 px-3 border"
-        >
-          <FaGithub className="h-4 w-4" />
+        <a href={siteConfig.links.github} target="_blank">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="bg-transparent hover:bg-muted/20 transition-all duration-300 p-2"
+          >
+            <FaGithub className="h-4 w-4" />
+          </Button>
         </a>
       </div>
 
-      {/* <DocsSidebar /> */}
+      <DocsSidebar />
       <div className="p-4 sm:px-8">{children}</div>
       {/* <div className="hidden lg:block pl-8 fixed w-64 top-24 right-0 bottom-0 overflow-y-auto">
         <TableOfContents />
