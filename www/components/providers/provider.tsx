a80db86f0fc3bb82300a0a26ff40734d729a7126
Astra <abhaypratapsinghwd@gmail.com>
2025-11-12 21:53:22 +0530

    bug fix

--- www/components/providers/provider.tsx
+++ www/components/providers/provider.tsx
@@ -12,6 +12,7 @@ interface ProviderProps {
 const LoadingFallback = () => {
   return (
     <div className="min-h-screen flex flex-col gap-4 items-center text-center justify-center relative overflow-hidden px-4">
+      <MaskedGridBackground />
       <div className="flex gap-4">
         <Image
           src={"/favicon.ico"}
