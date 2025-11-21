82e59a72217277adb77865e0813d1b99e5fea942
Astra <abhaypratapsinghwd@gmail.com>
2025-11-21 12:57:43 +0530

    Radial BG

--- www/app/docs/components/radial-background/index.ts
+++ www/app/docs/components/radial-background/index.ts
@@ -1,5 +1,6 @@
-export * from './radial-animated-background';
+export * from './radial-background-animated';
 export * from './radial-background-positions';
 export * from './radial-background-preview';
 export * from './radial-background-radius';
 export * from './radial-background-variants';
+
